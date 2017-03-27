39b4bdf7b5e5c4591768dcf5b27ab4b4e4136acc
FOURET JULIEN <julien.fouret@univ-lyon1.fr>
2017-03-27 17:16:36 +0200

    update version

--- src/gui/startMenu.java
+++ src/gui/startMenu.java
@@ -253,7 +253,7 @@ public class startMenu extends JFrame {
 		JTextPane txtpnHuh = new JTextPane();
 		txtpnHuh.setContentType("text/html");
 		txtpnHuh.setEditable(false);
-		txtpnHuh.setText("<html style=\"font-family: Monospace;\"> Author : Julien FOURET<br> Version 1.3.1 </html>");
+		txtpnHuh.setText("<html style=\"font-family: Monospace;\"> Author : Julien FOURET<br> Version 1.3.2 </html>");
 		txtpnHuh.setBounds(264, 298, 156, 49);
 		contentPane.add(txtpnHuh);
 		
