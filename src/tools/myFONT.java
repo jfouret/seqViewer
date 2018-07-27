4e2c5bfd73737d11d67e8a0e9be0d5dc52f60373
julien.fouret <julien.fouret@RTF100976.univ-lyon1.fr>
2018-07-27 22:26:10 +0200

    add licence information on each files

--- src/tools/myFONT.java
+++ src/tools/myFONT.java
@@ -1,3 +1,24 @@
+/*
+ *   seqViewer is a program dedicated to the visualization of sites under 
+ *   branch-site positive selection or similar approaches with superpositions 
+ *   with annotated UniProt features.
+ *   
+ *   Copyright (C) 2018  Julien Fouret
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package tools;
 
 import java.awt.Graphics2D;
